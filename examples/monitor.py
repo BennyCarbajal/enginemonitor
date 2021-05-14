b7e908875bdcc186a1993c85c6e2d0a6cf0f1b10
Benny Carbajal <benny.carbajalb@gmail.com>
2021-05-13 19:02:34 -0700

    monitor.py update

---
+++ examples/monitor.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+__author__ = "Benny Carbajal"
+__copyright__ = "Copyright 2021, ningh"
+__credits__ = [ "Benny Carbajal" ]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = [ "Benny Carbajal" ]
+__email__ = [ "benny.carbajalb@gmail.com" ]
+__status__ = "Beta"
+
+import json
+from enginemonitor import Engine
+
+def main() :
+ out = json.dumps( {
+  'basic': {
+   'ip': ngin.getIp(),
+   'computername': ngin.getComputer(),
+   'username': ngin.getUser(),
+   'cpu': ngin.getCpu(), 
+   'cores': ngin.getCores(),
+   'ram': ngin.getRam(),
+   'board': ngin.getBoard(),
+   'gpu': ngin.getGpu(),
+   'logical_disk': ngin.getDisks()
+  },
+  'sensors': ngin.getSensorBySpecs( "CPU", "Temperature" )
+ }, indent=2 )
+ print( out )
+
+if __name__ == '__main__':
+ ngin = Engine()
+ main()
