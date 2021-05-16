e7725758c1622585a331d23c0a1c6c1f6ca373ee
Benny Carbajal <benny.carbajalb@gmail.com>
2021-05-16 13:40:03 -0700

    init file

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
 from setuptools import setup
 
-def _post_install() :
- pass
-
-
-_post_install()
+setup(
+ name = 'enginemonitor',
+ version = '1.0.0',
+ license = 'license name, e.g. MIT',
+ description = '',
+ author = 'Benny Carbajal',
+ author_email = 'benny.carbajalb@gmail.com',
+ package_dir={'':'src'},
+ install_requires=[
+  'psutil',
+  'pymongo',
+  'wmi'
+ ],
+ classifiers=[
+  'Development Status :: 3 - Alpha',
+  'Programming Language :: Python :: 3.7',
+  'Natural Language :: English',
+  'Intended Audience :: Developers',
+  'License :: OSI Approved :: MIT License',
+  'Operating System :: Operating System :: OS Independent',
+  'Programming Language :: Python',
+  'Programming Language :: Python :: 3',
+  'Topic :: Utilities',
+ ]
+)
